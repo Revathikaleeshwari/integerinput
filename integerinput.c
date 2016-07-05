3a6c66cbe2c64ef0847dff5b59a2a541cf6b6221
Revathi N <nrevathibe38@gmail.com>
2016-07-05 08:57:05 +0530

    Create integerinput.c

---
+++ integerinput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d;
+clrscr();
+printf("Enter a integer:\n");
+scanf("%d%d%d",&a,&b,&c);
+d=a*a+b*b+c*c;
+printf("The output is :%d\n",d);
+return 0;
+}
